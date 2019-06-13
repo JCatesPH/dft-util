7b861b1e80aa4d88071f25124974be1ec6c622e3
Jalen M. Cates <jmcates@uahpc.ua.edu>
2019-06-12 19:14:40 -0500

    HPC updates

--- zmc-tf/zmc-tf-test1.py
+++ zmc-tf/zmc-tf-test1.py
@@ -9,8 +9,20 @@ import numpy as np
 import tensorflow as tf
 import ZMCintegral
 
+#from tensorflow_on_slurm import tf_config_from_slurm
+
+#cluster, my_job_name, my_task_index = tf_config_from_slurm(ps_number=1)
+#cluster_spec = tf.train.ClusterSpec(cluster)
+#server = tf.train.Server(server_or_cluster_def=cluster_spec,
+#                         job_name=my_job_name,
+#                         task_index=my_task_index)
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
 def testfoo(x):
-    return tf.math.sin(x[0]+x[1]+x[2]+x[3])
+    return tf.sin(x[0]+x[1]+x[2]+x[3])
 
 MC = ZMCintegral.MCintegral(testfoo, [[0,2], [1,4], [2,3], [4,6]])
 
